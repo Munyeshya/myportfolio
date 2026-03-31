98e48b478347bfa315583e72d39010caba7f7032
Munyeshya <obedbenny54@gmail.com>
2026-03-31 21:15:02 +0200

    dev

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import { motion } from 'framer-motion'
 import { profile } from '../data/profile'
 
 const navItems = [
-  ['Home', '#home'],
-  ['About', '#about'],
-  ['Skills', '#skills'],
-  ['Projects', '#projects'],
-  ['Resume', '#experience'],
-  ['Contact', '#contact'],
+  ['Home', '/#home'],
+  ['About', '/#about'],
+  ['Skills', '/#skills'],
+  ['Projects', '/#projects'],
+  ['Resume', '/#experience'],
+  ['Contact', '/#contact'],
 ]
 
 function Navbar() {
@@ -20,7 +20,7 @@ function Navbar() {
     >
       <div className="container">
         <div className="site-navbar d-flex align-items-center justify-content-between">
-          <a href="#home" className="site-logo text-decoration-none">
+          <a href="/#home" className="site-logo text-decoration-none">
             <span className="site-logo-mark">B</span>
             <span>{profile.name.split(' ')[1]} {profile.name.split(' ')[2]}</span>
           </a>
@@ -33,7 +33,7 @@ function Navbar() {
             ))}
           </nav>
 
-          <a href="#contact" className="talk-button text-decoration-none">
+          <a href="/#contact" className="talk-button text-decoration-none">
             Let&apos;s Talk
           </a>
         </div>
